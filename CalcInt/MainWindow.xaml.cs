d8736d5954b5dd3c58af3121ff554caa7f09eb1f
TatsuyaKashiwa <sachikaze417@gmail.com>
2024-10-05 21:17:45 +0900

    プロジェクト ファイルを追加します。

---
+++ CalcInt/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CalcInt
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        void ButtonInput(string s) 
+        {
+            if (Result.Content is "0")
+            {
+                Result.Content = s;
+            }
+            else
+            {
+                Result.Content += s;
+            }
+        }
+
+        private void seven_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonInput((string)seven.Content);
+        }
+
+        private void eight_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonInput((string)eight.Content);
+        }
+
+        private void nine_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonInput((string)nine.Content);
+        }
+
+        private void four_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonInput((string)four.Content);
+        }
+
+        private void five_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonInput((string)five.Content);
+        }
+
+        private void six_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonInput((string)six.Content);
+        }
+
+        private void one_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonInput((string)one.Content);
+        }
+
+        private void two_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonInput((string)two.Content);
+        }
+
+        private void three_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonInput((string)three.Content);
+        }
+
+        private void sign_Click(object sender, RoutedEventArgs e)
+        {
+            int result = int.Parse((string)Result.Content);
+            Result.Content = (-result).ToString();
+        }
+
+        private void zero_Click(object sender, RoutedEventArgs e)
+        {
+            ButtonInput((string)zero.Content);
+        }
+
+        private void clear_Click(object sender, RoutedEventArgs e)
+        {
+            Result.Content = "";
+        }
+    }
+}
