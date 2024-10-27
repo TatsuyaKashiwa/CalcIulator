fb4fc3aac208914db15f257ce234ee5288e28d1b
TatsuyaKashiwa <sachikaze417@gmail.com>
2024-10-27 16:19:15 +0900

    イベントハンドラをClickからCommandに変更,Nameの先頭をすべて大文字に変更

--- CalcInt/ResultsViewModel.cs
+++ CalcInt/ResultsViewModel.cs
@@ -10,9 +10,65 @@ namespace CalcInt
 {
     internal class ResultsViewModel : INotifyPropertyChanged
     {
+        private ResultModel rM;
+        public ResultsViewModel() 
+        {
+            ResultModel rM = new ResultModel();
+        }
         public event PropertyChangedEventHandler? PropertyChanged;
 
         protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "") 
             => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        public string BinaryResultContent 
+        { get {return rM.BinaryResultContent; }
+            set
+            {
+                if (rM.BinaryResultContent != value) 
+                { 
+                    rM.BinaryResultContent = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string HexadecimalResultContent
+        {
+            get { return rM.HexadecimalResultContent; }
+            set
+            {
+                if (rM.HexadecimalResultContent != value)
+                {
+                    rM.HexadecimalResultContent = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string PreviousResultContent
+        {
+            get { return rM.PreviousResultContent; }
+            set
+            {
+                if (rM.PreviousResultContent != value)
+                {
+                    rM.PreviousResultContent = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public string ResultContent
+        {
+            get { return rM.ResultContent; }
+            set
+            {
+                if (rM.ResultContent != value)
+                {
+                    rM.ResultContent = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
     }
 }
