ff6959c93bed679b0668e53d48d1aae6b58b2e1b
TatsuyaKashiwa <sachikaze417@gmail.com>
2024-10-27 15:24:38 +0900

    モデルのコストラクタ作成

---
+++ CalcInt/ResultModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcInt
+{
+    internal class ResultModel
+    {
+        public string BinaryResultContent { get; set; } = "";
+        public string HexadecimalResultContent { get; set; } = "";
+
+        public string PreviousResultContent { get; set; } = "";
+        public string ResultContent { get; set; } = "";
+    }
+}
--- CalcInt/ResultsViewModel.cs
+++ CalcInt/ResultsViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,6 +10,9 @@ namespace CalcInt
 {
     internal class ResultsViewModel : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "") 
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
     }
 }
